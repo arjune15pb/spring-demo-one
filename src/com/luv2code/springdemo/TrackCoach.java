fc888f384f23e870223e743b0cea655fe8269838
arjune15pb <karnarjun2000@gmail.com>
2022-11-14 15:57:22 +0530

    change cheking

---
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -0,0 +1,30 @@
+package com.luv2code.springdemo;
+
+public class TrackCoach implements Coach {
+   
+	private FortuneService fortuneService;
+	
+	public TrackCoach() {
+		
+	}
+	
+	public TrackCoach(FortuneService fortuneService) {
+	
+		this.fortuneService = fortuneService;
+	}
+
+	
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return "just Do it: "+fortuneService.getFortune();
+	}
+
+}
